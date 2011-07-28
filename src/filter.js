08bf75dc8db7b68b7e1968916577c397cb2912f2
mhweaver <mhweaver@gmail.com>
2011-07-28 18:13:53 +0300

    Got rid of jQuery, since this it's overkill for something this simple

--- src/filter.js
+++ src/filter.js
@@ -2,26 +2,54 @@ if (localStorage['showNSFW'] == undefined) {
   localStorage['showNSFW'] = true
 }
 
-$(document).ready(function() {
+document.onreadystatechange = function(e) {
+  if (document.readyState != "complete") return
   var showNSFW = localStorage['showNSFW']
   var showNSFWText = showNSFW == "true" ? 'on' : 'off'
-  if (showNSFW == 'false') $('.over18').hide()
+  if (showNSFW == 'false') {
+    map(document.querySelectorAll('.over18'), function(x) {console.log(x.style)})
+    map(document.querySelectorAll('.over18'), hide)
+  }
+ 
+  var toggleLink = document.createElement("a")
+  toggleLink.href = "javascript:void(0)"
+  toggleLink.id = "toggleNSFW"
+  toggleLink.innerText = "[NSFW " + showNSFWText + "]"
   
-  $('a.pref-lang').parent().parent()
-    .after('<a href="javascript:void(0);" id="toggleNSFW">[NSFW ' + showNSFWText + ']</a>')
-    .after('<span class="separator">|</span>')
+  var separator = document.createElement("span")
+  separator.className = "separator"
+  separator.innerText = "|"
+  
+  var preferences = document.querySelector('a.pref-lang').parentNode.parentNode
+  preferences.parentNode.insertBefore(toggleLink, preferences.nextSibling)
+  preferences.parentNode.insertBefore(separator, toggleLink)
+  
     
     
-  $('#toggleNSFW').click(function(e) {
+  document.querySelector('#toggleNSFW').addEventListener('click', function(e) {
     toggleNSFW()
-    $(this).text('[NSFW ' + (localStorage['showNSFW'] == "true" ? 'on' : 'off') + ']')
-  })
-})
+    this.innerText = '[NSFW ' + (localStorage['showNSFW'] == "true" ? 'on' : 'off') + ']'
+  }, false)
+}
 function toggleNSFW() {
   localStorage['showNSFW'] = localStorage['showNSFW'] == "true" ? "false" : "true"
   if (localStorage['showNSFW'] == "true") {
-    $('.over18').show()
+    map(document.querySelectorAll('.over18'), show)
   } else {
-    $('.over18').hide()
+    map(document.querySelectorAll('.over18'), hide)
   }
+}
+
+function map(xs, fn) {
+  var newArr = []
+  for (i in xs) {
+    newArr.push(fn(xs[i]))
+  }
+  return newArr
+}
+function hide(element) {
+  if (element.style != undefined) return element.style["display"] = "none"
+}
+function show(element) {
+  if (element.style != undefined) return element.style["display"] = "inline"
 }
