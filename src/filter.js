59aca9c0abf703473db441787865ad3a14306d68
mhweaver <mhweaver@gmail.com>
2011-08-04 13:27:48 +0300

    Added handler for 'hashchange' event to fix conflict with RES

--- src/filter.js
+++ src/filter.js
@@ -4,11 +4,8 @@ if (localStorage['showNSFW'] == undefined) {
 
 document.onreadystatechange = function(e) {
   if (document.readyState != "complete") return
-  var showNSFW = localStorage['showNSFW']
-  var showNSFWText = showNSFW == "true" ? 'on' : 'off'
-  if (showNSFW == 'false') {
-    document.querySelectorAll('.over18').map(hide)
-  }
+  var showNSFWText = (localStorage['showNSFW'] == "true") ? 'on' : 'off'
+  showHide()
  
   var toggleLink = document.createElement("a")
   toggleLink.href = "javascript:void(0)"
@@ -30,22 +27,20 @@ document.onreadystatechange = function(e) {
     this.innerText = '[NSFW ' + (localStorage['showNSFW'] == "true" ? 'on' : 'off') + ']'
   }, false)
 }
-function toggleNSFW() {
-  localStorage['showNSFW'] = localStorage['showNSFW'] == "true" ? "false" : "true"
-  if (localStorage['showNSFW'] == "true") {
-    document.querySelectorAll('.over18').map(show)
-  } else {
-    document.querySelectorAll('.over18').map(hide)
-  }
-}
 
 // Warning: Since this works with NodeLists, changes will be made to the actual
-// NodeList object (as opposed to Array.map() which returns a new Array object)
+// NodeList object (as opposed to Array.map() which returns a new Array
+// object), so this is NOT a pure function, unlike most map() functions
 NodeList.prototype.map = function(fn) {
   for (i in this) {
     fn(this[i])
   }
   return this
+}
+
+function toggleNSFW() {
+  localStorage['showNSFW'] = localStorage['showNSFW'] == "true" ? "false" : "true"
+  showHide()
 }
 function hide(element) {
   if (element.style != undefined) return element.style["display"] = "none"
@@ -53,3 +48,11 @@ function hide(element) {
 function show(element) {
   if (element.style != undefined) return element.style["display"] = "inline"
 }
+function showHide() {
+  if (localStorage['showNSFW'] == 'true') {
+    document.querySelectorAll('.over18').map(show)
+  } else {
+    document.querySelectorAll('.over18').map(hide)
+  }
+}
+window.addEventListener('hashchange', showHide)
